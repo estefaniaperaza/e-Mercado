9797b834837f1bea39dbc3be6e4f9a8e27489b31
estefaniaperaza <estefaniaperaza10@gmail.com>
2021-08-19 18:31:15 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -5,22 +5,26 @@ function showProducts(array) {
     for (let i = 0; i < array.length; i++){
 
         let product = array[i];
-        let imgSrc = "";
-
-               array[0].imgSrc = '<img height="200" src="img/prod1.jpg">'
-               array[1].imgSrc = '<img height="200" src="img/prod2.jpg">'
-               array[2].imgSrc = '<img height="200" src="img/prod3.jpg">'
-               array[3].imgSrc = '<img height="200" src="img/prod4.jpg">'
         
-
-        listado += "Nombre: " + product.name + "<br>";
-        listado += "Descripción: " + product.description + "<br>";
-        listado += "Precio: " + product.cost + "<br>";
-        listado += "Moneda: " + product.currency + "<br>";
-        listado += "Imagen: "  + "<br>";
-        listado += product.imgSrc + "<br>";
-        listado += "Vendidos: " + product.soldCount + "<br>";
-        listado += "<br><hr><br>"
+        
+        listado += ` 
+        <div class="row">
+        <div class="col-3">
+        <img src="` + product.imgSrc +  `" class="img-thumbnail">
+        </div>
+        <div class="col">
+        <div class="d-flex w-100 justify-content-between">
+        <h4 class="mb-1">`+ product.name + `</h4>
+        <small class="text-muted">`+ product.soldCount + ` vendidos</small> 
+        </div>
+        <p class="mb-1">` + product.description + `</p>
+        <p class="mb-1">` + product.currency + " " + product.cost +  `</p>
+        <br><br><br><hr><br><br>
+        </div>
+        </div>
+        
+        `
+        
     }
 
     
